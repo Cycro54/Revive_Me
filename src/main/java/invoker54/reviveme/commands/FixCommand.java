d78d43642e6a8e23ce19877c299fe24ce10d3181
Cycro54 <creatordebator@gmail.com>
2023-03-31 04:17:07 -0700

    Fixes - Fix command will no longer refresh your item list if it's filled already

--- src/main/java/invoker54/reviveme/commands/FixCommand.java
+++ src/main/java/invoker54/reviveme/commands/FixCommand.java
@@ -88,7 +88,8 @@ public class FixCommand {
             caller.stopUsingItem();
 
             //This will only happen if the caller is in a single caller world
-            if (caller.getServer().getPlayerList().getPlayers().size() == 1 && !cap.usedSacrificedItems()){
+            if (caller.getServer().getPlayerList().getPlayers().size() == 1 && !cap.usedSacrificedItems()
+            && cap.getItemList().size() == 0){
                 //Generate a sacrificial item list
                 ArrayList<Item> items = new ArrayList<>();
                 for (ItemStack itemStack : caller.getInventory().items) {
