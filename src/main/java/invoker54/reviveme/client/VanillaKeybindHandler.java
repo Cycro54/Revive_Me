415bb797921f94b61e24ee257df9a5e8ac7e49ea
Cycro54 <creatordebator@gmail.com>
2025-02-23 15:19:39 -0800

    1.20.1-4.1.3:
    Changes
    -Changed the license to LGPLv3
    
    Fixes
    -Keybinds now deactivate properly while in the dying state
    -Drop keybind now works while in the dying state

--- src/main/java/invoker54/reviveme/client/VanillaKeybindHandler.java
+++ src/main/java/invoker54/reviveme/client/VanillaKeybindHandler.java
@@ -21,7 +21,7 @@ public class VanillaKeybindHandler {
         Options options = ClientUtil.getMinecraft().options;
 
         vanillaKeyMappings = List.of(ArrayUtils.addAll(new KeyMapping[]{
-                /*options.keyAttack, options.keyUse,*/ options.keyUp, options.keyLeft, options.keyDown, options.keyRight, options.keyJump, options.keyShift, options.keySprint, /*options.keyDrop,*/ options.keyInventory, options.keyChat, options.keyPlayerList, options.keyPickItem, options.keyCommand, options.keySocialInteractions, options.keyScreenshot, options.keyTogglePerspective, options.keySmoothCamera, options.keyFullscreen, options.keySpectatorOutlines, options.keySwapOffhand, options.keySaveHotbarActivator, options.keyLoadHotbarActivator, options.keyAdvancements}, options.keyHotbarSlots));
+                /*options.keyAttack, options.keyUse,*/ options.keyUp, options.keyLeft, options.keyDown, options.keyRight, options.keyJump, options.keyShift, options.keySprint, options.keyDrop, options.keyInventory, options.keyChat, options.keyPlayerList, options.keyPickItem, options.keyCommand, options.keySocialInteractions, options.keyScreenshot, options.keyTogglePerspective, options.keySmoothCamera, options.keyFullscreen, options.keySpectatorOutlines, options.keySwapOffhand, options.keySaveHotbarActivator, options.keyLoadHotbarActivator, options.keyAdvancements}, options.keyHotbarSlots));
 
         movementMappings = List.of(options.keyShift, options.keyLeft, options.keyUp, options.keyRight, options.keyDown);
 //        LOGGER.error("Vanilla keybindings new size: " + vanillaKeyMappings.size());
